e106766c6a2ef5363e44255123373056ac47103d
Igor Kononenko <i.kononenko@yadro.com>
2021-06-02 18:08:09 +0300

    Add 'Server' Node
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

---
+++ src/definitions.hpp
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2021 YADRO
+
+#ifndef __SYSTEM_DEFINITIONS_H__
+#define __SYSTEM_DEFINITIONS_H__
+
+namespace app
+{
+namespace entity
+{
+namespace obmc
+{
+namespace definitions
+{
+
+constexpr const char* entityServer = "Server";
+constexpr const char* fieldName = "Name";
+constexpr const char* fieldType = "Type";
+constexpr const char* fieldModel = "Model";
+constexpr const char* fieldManufacturer = "Manufacturer";
+constexpr const char* fieldSerialNumber = "SerialNumber";
+constexpr const char* fieldPartNumber = "PartNumber";
+constexpr const char* fieldDatetime = "Datetime";
+
+constexpr const char* metaFieldPrefix = "__meta_";
+
+constexpr const char* metaObjectPath = "__meta_field__object_path";
+constexpr const char* metaObjectService = "__meta_field__object_service";
+
+
+namespace version
+{
+constexpr const char* fieldVersionBios = "BiosVersion";
+constexpr const char* fieldVersionBmc = "BmcVersion";
+} // namespace version
+
+
+namespace supplement_providers
+{
+
+namespace relations
+{
+constexpr const char* providerRelations = "Relations";
+constexpr const char* fieldAssociations = "Associations";
+constexpr const char* fieldEndpoint = "Endpoint";
+constexpr const char* fieldSource = "Source";
+constexpr const char* fieldDestination = "Destination";
+
+} // namespace relations
+
+namespace status
+{
+constexpr const char* providerStatus = "__provider__Status";
+
+constexpr const char* fieldStatus = "Status";
+constexpr const char* fieldObjectCauthPath = "Causer";
+
+} // namespace status
+namespace version
+{
+constexpr const char* providerVersion = "__provider__Version";
+constexpr const char* fieldVersion = "Version";
+constexpr const char* fieldPurpose = "Purpose";
+} // namespace version
+
+namespace assetTag
+{
+constexpr const char* providerAssetTag = "__provider__AssetTag";
+constexpr const char* fieldTag = "AssetTag";
+} // namespace assetTag
+
+namespace indicatorLed
+{
+constexpr const char* providerIndicatorLed = "__provider__IndicatorLed";
+constexpr const char* fieldLed = "Led";
+} // namespace assetTag
+
+
+} // namespace supplement_providers
+
+} // namespace definitions
+} // namespace obmc
+} // namespace entity
+} // namespace app
+#endif // __SYSTEM_DEFINITIONS_H__
--- src/queries.cpp
+++ src/queries.cpp
@@ -21,6 +21,81 @@ namespace core
 
 void Application::initEntityMap()
 {
+    using namespace app::entity;
+    using namespace app::entity::obmc;
+    using namespace app::query;
+    using namespace app::query::obmc;
+    using namespace definitions::supplement_providers;
+
+    using namespace std::literals;
+
+    /* Define GLOBAL STATUS supplement provider */
+    entityManager.buildSupplementProvider(status::providerStatus)
+        ->addMembers({
+            status::fieldStatus,
+            status::fieldObjectCauthPath,
+        })
+        .addQuery<dbus::DBusQueryBuilder>(dbusBrokerManager)
+        ->addObject<Status>()
+        .complete();
+
+    /* Define GLOBAL VERSION supplement provider */
+    entityManager
+        .buildSupplementProvider(
+            definitions::supplement_providers::version::providerVersion)
+        ->addMembers({
+            definitions::supplement_providers::version::fieldPurpose,
+            definitions::supplement_providers::version::fieldVersion,
+        })
+        .addQuery<dbus::DBusQueryBuilder>(dbusBrokerManager)
+        ->addObject<Version>()
+        .complete();
+
+    /* Define SYSTEM ASSET TAG supplement provider */
+    entityManager
+        .buildSupplementProvider(
+            definitions::supplement_providers::assetTag::providerAssetTag)
+        ->addMembers({
+            definitions::supplement_providers::assetTag::fieldTag,
+        })
+        .addQuery<dbus::DBusQueryBuilder>(dbusBrokerManager)
+        ->addObject<AssetTag>()
+        .complete();
+
+    /* Define SYSTEM INDICATOR LED supplement provider */
+    entityManager
+        .buildSupplementProvider(definitions::supplement_providers::
+                                     indicatorLed::providerIndicatorLed)
+        ->addMembers({
+            definitions::supplement_providers::indicatorLed::fieldLed,
+        })
+        .addQuery<dbus::DBusQueryBuilder>(dbusBrokerManager)
+        ->addObject<IndicatorLed>()
+        .complete();
+    /* Define SERVER entity */
+    entityManager.buildEntity(definitions::entityServer)
+        ->addMembers({
+            definitions::fieldName,
+            definitions::fieldType,
+            definitions::fieldManufacturer,
+            definitions::fieldModel,
+            definitions::fieldPartNumber,
+            definitions::fieldSerialNumber,
+            definitions::version::fieldVersionBios,
+            definitions::version::fieldVersionBmc,
+            definitions::fieldDatetime,
+        })
+        .linkSupplementProvider(
+            definitions::supplement_providers::version::providerVersion,
+            std::bind(&Server::linkVersions, _1, _2))
+        .linkSupplementProvider(indicatorLed::providerIndicatorLed,
+                                std::bind(&Server::linkIndicatorLed, _1, _2))
+        .linkSupplementProvider(assetTag::providerAssetTag)
+        .linkSupplementProvider(status::providerStatus,
+                                std::bind(&Server::linkStatus, _1, _2))
+        .addQuery<dbus::DBusQueryBuilder>(dbusBrokerManager)
+        ->addObject<Server>()
+        .complete();
 }
 
 } // namespace core
