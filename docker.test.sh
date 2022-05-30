95bc3b125ec3b589287b580949164c34b726a77b
Igor Kononenko <i.kononenko@yadro.com>
2022-05-30 16:32:49 +0300

    Docker:testing:Update docker-container start script
    Add forwarding of the systemd-journal socket to make the
    phosphor-logging engine available, and to passt the journal entry
    through from the continer to the host system.
    
    End-user-impact: None (the changes help to test yaweb in docker)
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

--- docker.test.sh
+++ docker.test.sh
@@ -1,29 +1,70 @@
-#!/bin/bash -exu
+#!/bin/bash -eu
 
-# docker build --no-cache --force-rm -t obmc-webapp-server -f Dockerfile .
+function run_container() {
+  DOCKER_IMAGE="obmc-webapp-server"
 
-DOCKER_IMAGE="obmc-webapp-server"
+  CID=
+  function on_exit {
+    # Completely remove container after usage
+    [[ -z "${CID}" ]] || docker rm --force --volumes "${CID}" > /dev/null
+  }
+  trap on_exit EXIT
 
-CID=
-function on_exit {
-  # Completely remove container after usage
-  [[ -z "${CID}" ]] || docker rm --force --volumes ${CID} > /dev/null
+  SOURCE_DIR="${SOURCE_DIR:-$(realpath $(dirname ${BASH_SOURCE[0]}))}"
+  [[ -d ${DLCACHE_DIR:-} ]] && DLCACHE_OPT="--volume ${DLCACHE_DIR}:${DLCACHE_DIR}:Z" || DLCACHE_OPT=
+  [[ -t 1 ]] && PASSTTY_OPT="--tty" || PASSTTY_OPT=
+
+  CID=$(docker create \
+              ${PASSTTY_OPT} \
+              --interactive \
+              --env HOME=/source/build/ \
+              --env "SSH_AUTH_SOCK=/ssh-agent" \
+              --tmpfs /tmp \
+              --tmpfs /run \
+              --tmpfs /run/lock \
+              --volume "${SOURCE_DIR}":/source:Z \
+              --volume "${SSH_AUTH_SOCK}":/ssh-agent \
+              --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
+              --volume /run/systemd/journal/socket:/run/systemd/journal/socket \
+              --publish 18081:18081 \
+              --publish 30022:22 \
+              --privileged \
+              ${DLCACHE_OPT} \
+              --workdir "${SOURCE_DIR}" \
+              "${DOCKER_IMAGE}" \
+              "$@")
+
+  docker start --attach --interactive ${CID}
 }
-trap on_exit EXIT
 
-SOURCE_DIR="${SOURCE_DIR:-$(realpath $(dirname ${BASH_SOURCE[0]}))}"
-[[ -d ${DLCACHE_DIR:-} ]] && DLCACHE_OPT="--volume ${DLCACHE_DIR}:${DLCACHE_DIR}:Z" || DLCACHE_OPT=
-[[ -t 1 ]] && PASSTTY_OPT="--tty" || PASSTTY_OPT=
+function build_docker_images() {
+  echo "Build 'obmc-webapp-base' image..."
+  docker build --no-cache --force-rm -t obmc-webapp-base -f Dockerfile.base .
+  echo "Build 'obmc-webapp-server' image..."
+  docker build --no-cache --force-rm -t obmc-webapp-server -f Dockerfile .
+  echo "Building obmc-webapp docker images are successful."
+}
 
-CID=$(docker create \
-             ${PASSTTY_OPT} \
-             --interactive \
-             --env HOME=/source/build/ \
-             --volume ${SOURCE_DIR}:/source:Z \
-             -p 18081:18081 \
-             ${DLCACHE_OPT} \
-             --workdir ${SOURCE_DIR} \
-             ${DOCKER_IMAGE} \
-             $@)
+COMMAND=${1:-"run"}
 
-docker start --attach --interactive ${CID}
+case "${COMMAND}" in
+  run)
+    mkdir -p build
+    run_container "$@"
+    exit
+    ;;
+
+  build)
+    build_docker_images
+    exit
+    ;;
+  help)
+    echo "Usage: ${0} run|build|help"
+    exit
+    ;;
+  *)
+    echo "Unknown command: ${COMMAND}" >&1
+    exit
+    ;;
+
+esac
