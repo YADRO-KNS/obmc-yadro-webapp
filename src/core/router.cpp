519dfe7d34847c2254af3976384b35d0cd1bf832
Igor Kononenko <i.kononenko@yadro.com>
2022-08-23 00:57:02 +0300

    core:router: Add DynamicRouter rule.
    Current core:Router can comparing only full path of given URI, that is
    handler registried for.
    Add DynamicRouterRule that is provide complex search appropriate handler
    that is defines as callback.
    
    This path is initial point to support DMTF REDFISH.
    
    End-user-impact: None (add internal API to support complex HTTP
                     Protocols, e.g. DMTF REDFISH)
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

--- src/core/router.cpp
+++ src/core/router.cpp
@@ -19,13 +19,12 @@ namespace core
 using namespace phosphor::logging;
 
 Router::RouteMap Router::routerHandlers;
+Router::DynamicRouteMap Router::dynamicRouterHandlers;
 
-Router::Router(const RequestPtr& request) :
-    requestObject(request),
-    responseObject(std::make_unique<app::core::Response>())
+Router::Router(const RequestPtr& request) : requestObject(request)
 {}
 
-const ResponseUni& Router::process()
+const ResponsePtr Router::process()
 {
     // Current session sharing architecture between BMCWEB and WEBAPP processes,
     // in fact, works via filesystem synchronization. Hence, we need to re-read
@@ -41,48 +40,66 @@ const ResponseUni& Router::process()
     }
 
     if (!this->handler) {
-        auto builder = routerHandlers.find(getRequest()->environment().requestUri);
-        if (builder == routerHandlers.end())
-        {
-            return getResponse();
-        }
-        this->handler = builder->second(builder->first);
+        auto response = std::make_shared<app::core::Response>();
+        setGeneralHeaders(response);
+        response->setStatus(http::statuses::Code::NotFound);
+        return response;
     }
 
-    handler->run(getRequest(), getResponse());
-    setGeneralHeaders();
-    log<level::DEBUG>(getResponse()->getHead().c_str());
-    return getResponse();
-}
-
-void Router::setGeneralHeaders()
-{
-    using namespace app::http;
-    constexpr const char* headerDateFormat = "%a, %d %b %Y %H:%M:%S GMT";
-    getResponse()->setHeader(headers::contentLength,
-                             std::to_string(getResponse()->totalSize()));
-    getResponse()->setHeader(
-        headers::date,
-        app::helpers::utils::getFormattedCurrentDate(headerDateFormat));
+    
+    auto response = handler->run(getRequest());
+    setGeneralHeaders(response);
+    log<level::DEBUG>(response->getHead().c_str());
+    return response;
 }
 
 bool Router::preHandler()
 {
-    if (!this->handler)
+    if (this->handler)
     {
-        auto builder =
-            routerHandlers.find(getRequest()->environment().requestUri);
-        if (builder == routerHandlers.end())
-        {
-            // Route no found. Immediate exit, but give the chanse to handle
-            // that for the process()
-            log<level::DEBUG>("Route no found. Pass throught");
-            return true;
-        }
-        this->handler = builder->second(builder->first);
+        handler->preHandlers(getRequest());
     }
 
-    return handler->preHandlers(getRequest());
+    // Static route pattern
+    // Lookup for static routes at first due in fact that is simple comparing
+    // O(log N) of std::size_t types.
+    const std::string uri = helpers::utils::toLower(getRequest()->getUriPath());
+    auto hashPattern = std::hash<std::string>{}(uri);
+    auto builder = routerHandlers.find(hashPattern);
+    if (builder != routerHandlers.end())
+    {
+        this->handler = builder->second(uri);
+        return handler->preHandlers(getRequest());
+    }
+
+    // Dynamic route pattern
+    // This is complex operation to find relevant route handler, match callback
+    // will be called of each IDynamicRouteHandler until it will be found.
+    for (auto& [condition, builder] : dynamicRouterHandlers)
+    {
+        if (condition(getRequest()->environment().pathInfo))
+        {
+            this->handler = builder(getRequest());
+            return this->handler->preHandlers(getRequest());
+        }
+    }
+
+    // Route no found. Immediate exit, but give a chanse to handle that for the
+    // process()
+    log<level::DEBUG>("Route no found. Pass throught",
+                      entry("PATH=%s", uri.c_str()));
+    return true;
+}
+
+void Router::setGeneralHeaders(const ResponsePtr response)
+{
+    using namespace app::http;
+    constexpr const char* headerDateFormat = "%a, %d %b %Y %H:%M:%S GMT";
+    response->setHeader(headers::contentLength,
+                        std::to_string(response->totalSize()));
+    response->setHeader(
+        headers::date,
+        app::helpers::utils::getFormattedCurrentDate(headerDateFormat));
 }
 
 } // namespace core
