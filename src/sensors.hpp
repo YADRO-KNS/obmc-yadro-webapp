ca98daa199ca5c8c0f6d079f7bb722493ccf0d0d
Igor Kononenko <i.kononenko@yadro.com>
2022-08-23 01:29:39 +0300

    entity: sensor: Refine Entity Sensors.
    End-user-impact: Now Sensor GraphQL object have following additional
                     fields:
                     * MaxValue
                     * MinValue
                     * LowCritical
                     * LowWarning
                     * HighWarning
                     * HighCritical
                     * State
                     * Relation to appropriate Invenotry items:
                       + Fan
                       + Processor
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

--- src/sensors.hpp
+++ src/sensors.hpp
@@ -21,28 +21,63 @@ namespace entity
 using namespace app::entity;
 using namespace app::query;
 using namespace phosphor::logging;
-
+using namespace app::helpers::utils;
 class Sensors :
     public entity::Collection,
     public NamedEntity<Sensors>,
     public CachedSource
 {
   public:
-    static constexpr const char* fieldName = "Name";
-    static constexpr const char* fieldValue = "Reading";
-    static constexpr const char* fieldUnit = "Unit";
-    static constexpr const char* fieldLowCritical = "LowCritical";
-    static constexpr const char* fieldLowWarning = "LowWarning";
-    static constexpr const char* fieldHighWarning = "HighWarning";
-    static constexpr const char* fieldHighCritical = "HighCritical";
+    enum class Group
+    {
+        power,
+        thermal,
+        sensors,
+    };
 
-    static constexpr const char* fieldWarningAlarmHigh = "WarningAlarmHigh";
-    static constexpr const char* fieldWarningAlarmLow = "WarningAlarmLow";
-    static constexpr const char* fieldCriticalAlarmHigh = "CriticalAlarmHigh";
-    static constexpr const char* fieldCriticalAlarmLow = "CriticalAlarmLow";
+    enum class Unit
+    {
+        temperature,
+        voltage,
+        rotational,
+        altitude,
+        current,
+        power,
+        energyJoules,
+        percent,
+        unkown
+    };
 
-    static constexpr const char* fieldAvailable = "Available";
-    static constexpr const char* fieldFunctional = "Functional";
+    enum class State
+    {
+        enabled,
+        absent
+    };
+
+    ENTITY_DECL_FIELD(std::string, Id)
+    ENTITY_DECL_FIELD(std::string, Name)
+    ENTITY_DECL_FIELD_DEF(double, Reading, 0)
+    ENTITY_DECL_FIELD_ENUM(Unit, Unit, unkown)
+    ENTITY_DECL_FIELD_ENUM(Group, Group, sensors)
+
+    ENTITY_DECL_FIELD_DEF(double, MaxValue, 0)
+    ENTITY_DECL_FIELD_DEF(double, MinValue, 0)
+    ENTITY_DECL_FIELD_DEF(double, LowCritical, 0)
+    ENTITY_DECL_FIELD_DEF(double, LowWarning, 0)
+    ENTITY_DECL_FIELD_DEF(double, HighWarning, 0)
+    ENTITY_DECL_FIELD_DEF(double, HighCritical, 0)
+
+    ENTITY_DECL_FIELD_DEF(double, WarningAlarmHigh, 0)
+    ENTITY_DECL_FIELD_DEF(double, WarningAlarmLow, 0)
+    ENTITY_DECL_FIELD_DEF(double, CriticalAlarmHigh, 0)
+    ENTITY_DECL_FIELD_DEF(double, CriticalAlarmLow, 0)
+
+    ENTITY_DECL_FIELD_DEF(bool, Available, false)
+    ENTITY_DECL_FIELD_DEF(bool, Functional, false)
+    
+    ENTITY_DECL_FIELD_ENUM(State, State, absent)
+    ENTITY_DECL_FIELD_ENUM(StatusProvider::Status, Status, ok)
+    ENTITY_DECL_FIELD(std::string, AssociatedInventoryId)
 
     class SensorQuery final : public dbus::FindObjectDBusQuery
     {
@@ -56,6 +91,8 @@ class Sensors :
             "xyz.openbmc_project.State.Decorator.Availability";
         static constexpr const char* sensorOperationalStatusInterface =
             "xyz.openbmc_project.State.Decorator.OperationalStatus";
+        static constexpr const char* assocDefIface =
+            "xyz.openbmc_project.Association.Definitions";
 
         static constexpr const char* propCriticalLow = "CriticalLow";
         static constexpr const char* propCriticalHigh = "CriticalHigh";
@@ -71,27 +108,137 @@ class Sensors :
         static constexpr const char* defaultUnitLabel = "Unit";
 
       private:
+        class FormatAssociatedInventory : public query::dbus::IFormatter
+        {
+          public:
+            ~FormatAssociatedInventory() override = default;
+
+            const DbusVariantType format(const PropertyName& property,
+                                         const DbusVariantType& value) override
+            {
+                constexpr const std::array inventorySlugList {
+                    "inventory",
+                    "cpu",
+                };
+                if (std::holds_alternative<DBusAssociationsType>(value))
+                {
+                    const auto associations =
+                        std::get<DBusAssociationsType>(value);
+                    for (const auto& association : associations)
+                    {
+                        const auto purpose = std::get<0>(association);
+                        if (std::end(inventorySlugList) !=
+                            std::find(std::begin(inventorySlugList),
+                                      std::end(inventorySlugList), purpose))
+                        {
+                            const auto inventory = std::get<2>(association);
+                            return getNameFromLastSegmentObjectPath(inventory,
+                                                                    false);
+                        }
+                    }
+                }
+                return std::string(EntityMember::fieldValueNotAvailable);
+            }
+        };
+        class GroupParser
+        {
+            const DBusInstancePtr instance;
+
+            enum class Index
+            {
+                voltage = 0,
+                power,
+                current,
+                fanTach,
+                temperature,
+                fanPwm,
+                utilization,
+                max
+            };
+            
+            inline size_t index(Index index)
+            {
+                return static_cast<size_t>(index);
+            }
+          public:
+            GroupParser(const DBusInstancePtr& instance): instance(instance) {}
+            ~GroupParser() = default;
+
+            Group operator()()
+            {
+                constexpr const size_t maxIndexes = static_cast<size_t>(Index::max);
+                static constexpr std::array<const char*, maxIndexes> paths = {
+                    "/xyz/openbmc_project/sensors/voltage/",
+                    "/xyz/openbmc_project/sensors/power/",
+                    "/xyz/openbmc_project/sensors/current/",
+                    "/xyz/openbmc_project/sensors/fan_tach/",
+                    "/xyz/openbmc_project/sensors/temperature/",
+                    "/xyz/openbmc_project/sensors/fan_pwm/",
+                    "/xyz/openbmc_project/sensors/utilization/",
+                };
+
+                static const std::map<Group, std::vector<const char*>> groups =
+                    {
+                        {
+                            Group::power,
+                            {
+                                paths[index(Index::voltage)],
+                                paths[index(Index::power)],
+                            },
+                        },
+                        {
+                            Group::thermal,
+                            {
+                                paths[index(Index::fanTach)],
+                                paths[index(Index::temperature)],
+                                paths[index(Index::fanPwm)],
+                            },
+                        },
+                    };
+
+                const auto& objectPath = instance->getObjectPath();
+                for (const auto& [group, identity] : groups)
+                {
+                    auto found =
+                        std::any_of(identity.begin(), identity.end(),
+                                    [objectPath](auto&& value) -> bool {
+                                        return objectPath.starts_with(value);
+                                    });
+                    if (found)
+                    {
+                        return group;
+                    }
+                }
+                return Group::sensors;
+            }
+        };
+
         class FormatUnit : public query::dbus::IFormatter
         {
+            inline const std::string longDBusEnum(const std::string& enumStr)
+            {
+                return "xyz.openbmc_project.Sensor.Value.Unit." + enumStr;
+            }
+
           public:
             ~FormatUnit() override = default;
 
             const DbusVariantType format(const PropertyName& property,
                                          const DbusVariantType& value) override
             {
-                static const StringFormatterDict units{
-                    {"xyz.openbmc_project.Sensor.Value.Unit.DegreesC", "°C"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.Volts", "Volts"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.RPMS", "RPM"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.Meters", "Meters"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.Amperes",
-                     "Amperes"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.Watts", "Watts"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.Joules", "Joules"},
-                    {"xyz.openbmc_project.Sensor.Value.Unit.Percent", "%"},
+                static const std::map<std::string, Unit> units{
+                    {longDBusEnum("DegreesC"), Unit::temperature},
+                    {longDBusEnum("Volts"), Unit::voltage},
+                    {longDBusEnum("RPMS"), Unit::rotational},
+                    {longDBusEnum("Meters"), Unit::altitude},
+                    {longDBusEnum("Amperes"), Unit::current},
+                    {longDBusEnum("Watts"), Unit::power},
+                    {longDBusEnum("Joules"), Unit::energyJoules},
+                    {longDBusEnum("Percent"), Unit::percent},
                 };
 
-                return formatStringValueFromDict(units, property, value);
+                return formatValueFromDict(units, property, value,
+                                           Unit::unkown);
             }
         };
 
@@ -103,7 +250,9 @@ class Sensors :
         DBUS_QUERY_DECL_EP(
             DBUS_QUERY_EP_IFACES(
                 sensorValueInterface,
-                DBUS_QUERY_EP_FIELDS_ONLY(propValue, fieldValue),
+                DBUS_QUERY_EP_FIELDS_ONLY(propValue, fieldReading),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldMinValue),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldMaxValue),
                 DBUS_QUERY_EP_SET_FORMATTERS2(fieldUnit,
                                               DBUS_QUERY_EP_CSTR(FormatUnit))),
             DBUS_QUERY_EP_IFACES(
@@ -121,7 +270,13 @@ class Sensors :
             DBUS_QUERY_EP_IFACES(sensorAvailabilityInterface,
                                  DBUS_QUERY_EP_FIELDS_ONLY2(fieldAvailable)),
             DBUS_QUERY_EP_IFACES(sensorOperationalStatusInterface,
-                                 DBUS_QUERY_EP_FIELDS_ONLY2(fieldFunctional)))
+                                 DBUS_QUERY_EP_FIELDS_ONLY2(fieldFunctional)),
+            DBUS_QUERY_EP_IFACES(
+                assocDefIface,
+                DBUS_QUERY_EP_SET_FORMATTERS(
+                    "Associations", fieldAssociatedInventoryId,
+                    DBUS_QUERY_EP_CSTR(FormatAssociatedInventory))))
+
       protected:
         DBUS_QUERY_DECLARE_CRITERIA(
             "/xyz/openbmc_project/sensors",
@@ -129,28 +284,27 @@ class Sensors :
 
         void setSensorName(const DBusInstancePtr& instance) const
         {
-            auto& objectPath = instance->getObjectPath();
-            instance->supplementOrUpdate(
-                fieldName,
-                app::helpers::utils::getNameFromLastSegmentObjectPath(
-                    objectPath));
+            const auto& objectPath = instance->getObjectPath();
+            setFieldId(instance, getNameFromLastSegmentObjectPath(objectPath, false));
+            setFieldName(instance, getNameFromLastSegmentObjectPath(objectPath));
         }
 
         void setStatus(const DBusInstancePtr& instance) const
         {
-            std::string status = StatusProvider::OK;
+            using AlarmStatusDict =
+                std::map<StatusProvider::Status, std::vector<std::string>>;
+            auto status = StatusProvider::Status::ok;
             DBusInstanceWeak instanceWeak(instance);
-            static const std::map<std::string, std::vector<std::string>>
-                alarmFieldsDict{
-                    {
-                        StatusProvider::critical,
-                        {fieldCriticalAlarmHigh, fieldCriticalAlarmLow},
-                    },
-                    {
-                        StatusProvider::warning,
-                        {fieldWarningAlarmHigh, fieldWarningAlarmLow},
-                    },
-                };
+            static const AlarmStatusDict alarmFieldsDict{
+                {
+                    StatusProvider::Status::ok,
+                    {fieldCriticalAlarmHigh, fieldCriticalAlarmLow},
+                },
+                {
+                    StatusProvider::Status::warning,
+                    {fieldWarningAlarmHigh, fieldWarningAlarmLow},
+                },
+            };
             const auto compareAlarmFn =
                 [instanceWeak](const std::string& value) -> bool {
                 auto instance = instanceWeak.lock();
@@ -164,7 +318,7 @@ class Sensors :
                 catch (std::bad_variant_access& ex)
                 {
                     // Several sensors might haven't critical/warning
-                    // property, so feel is their state is OK.
+                    // property, so feel is their status is OK.
                     log<level::DEBUG>(
                         "Fail to calculate sensor status",
                         entry("SENSOR=%s", instance->getField(fieldName)
@@ -184,24 +338,105 @@ class Sensors :
                     break;
                 }
             }
-            instance->supplementOrUpdate(StatusProvider::fieldStatus, status);
+            setFieldStatus(instance, status);
+        }
+
+        void setGroup(const DBusInstancePtr& instance) const
+        {
+            GroupParser parser(instance);
+            setFieldGroup(instance, parser());
+        }
+
+        void setState(const DBusInstancePtr& instance) const
+        {
+            setFieldState(instance, State::absent);
+            try
+            {
+                if (instance && instance->getField(fieldAvailable)->getBoolValue())
+                {
+                    setFieldState(instance, State::enabled);
+                }
+            }
+            catch (std::bad_variant_access& ex)
+            {
+                log<level::DEBUG>(
+                    "Fail to calculate sensor state",
+                    entry("SENSOR=%s", instance->getField(fieldName)
+                                           ->getStringValue()
+                                           .c_str()),
+                    entry("FIELD=%s", fieldAvailable),
+                    entry("ERROR=%s", ex.what()));
+            }
         }
 
         void supplementByStaticFields(const DBusInstancePtr& instance) const override
         {
             this->setSensorName(instance);
             this->setStatus(instance);
+            this->setState(instance);
+            this->setGroup(instance);
         }
     };
 
   public:
     Sensors() : Collection(), query(std::make_shared<SensorQuery>())
     {
+        this->createMember(fieldId);
         this->createMember(fieldName);
-        this->createMember(StatusProvider::fieldStatus);
+        this->createMember(fieldStatus);
+        this->createMember(fieldState);
+        this->createMember(fieldGroup);
     }
     ~Sensors() override = default;
 
+    static const IRelation::RelationRulesList& inventoryRelation()
+    {
+        static const IRelation::RelationRulesList relations{
+            {
+                fieldId,
+                Sensors::fieldAssociatedInventoryId,
+                [](const IEntityMember::InstancePtr& instance,
+                   const IEntityMember::IInstance::FieldType& value) -> bool {
+                    try
+                    {
+                        if (std::holds_alternative<std::string>(value) &&
+                            std::holds_alternative<std::string>(
+                                instance->getValue()))
+                        {
+                            const auto fanName = instance->getStringValue();
+                            const auto sensorInventory =
+                                std::get<std::string>(value);
+                            return fanName == sensorInventory;
+                        }
+                    }
+                    catch (std::exception& ex)
+                    {
+                        log<level::ERR>(
+                            "Fail to configure relation Inventory to Reading sensor",
+                            entry("ERROR=%s", ex.what()));
+                    }
+
+                    return false;
+                },
+            },
+        };
+        return relations;
+    }
+
+    static const ConditionPtr availableSensors()
+    {
+        return Condition::buildEqual(fieldAvailable, true);
+    }
+    
+    static const ConditionPtr sensorsByType(Unit unit)
+    {
+        return Condition::buildEqual(fieldUnit, unit);
+    }
+
+    static const ConditionPtr genericSensors()
+    {
+        return Condition::buildEqual(fieldGroup, Group::sensors);
+    }
   protected:
     ENTITY_DECL_QUERY(query)
   private:
