367dbf3be92cba4c8c4e02197ae3462b5e5ba0a0
Igor Kononenko <i.kononenko@yadro.com>
2022-08-23 00:57:02 +0300

    core:router: Add DynamicRouter rule.
    Current core:Router can comparing only full path of given URI, that is
    handler registried for.
    Add DynamicRouterRule that is provide complex search appropriate handler
    that is defines as callback.
    
    This path is initial point to support DMTF REDFISH.
    
    End-user-impact: None (add internal API to support complex HTTP
                     Protocols, e.g. DMTF REDFISH)
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

--- src/core/router.hpp
+++ src/core/router.hpp
@@ -5,9 +5,12 @@
 
 #include <core/request.hpp>
 #include <core/response.hpp>
+#include <core/helpers/utils.hpp>
 
 #include <memory>
 #include <string>
+
+#include <phosphor-logging/log.hpp>
 
 namespace app
 {
@@ -19,21 +22,48 @@ class Router;
 
 using RouteHandlerPtr = std::shared_ptr<IRouteHandler>;
 using RouteUni = std::unique_ptr<Router>;
+using UriSegments = std::vector<std::string>;
+
+using namespace phosphor::logging;
 
 class IRouteHandler
 {
   public:
-    virtual void run(const RequestPtr& request, ResponseUni& response) = 0;
+    virtual const ResponsePtr run(const RequestPtr& request) = 0;
     virtual bool preHandlers(const RequestPtr& request) = 0;
-
+    /**
+     * @brief Get the URI of handling resource
+     * 
+     * @return const std::string& - URI
+     */
+    // virtual const std::string& getUriPath() const = 0;
     virtual ~IRouteHandler() = default;
+};
+
+class IDynamicRouteHandler
+{
+  public:
+    /**
+     * @brief Get the pattern to dynamic expanding URI
+     * 
+     * @return const std::string& - pattern of dynamic URI
+     */
+    // virtual const std::string& getPattern() const = 0;
+
+    virtual ~IDynamicRouteHandler() = default;
 };
 
 class Router
 {
-    using RoutePattern = std::string;
-    using RouteHandlerBuilderFn = std::function<RouteHandlerPtr(const std::string&)>;
+    using RoutePattern = std::size_t;
+    using RouteHandlerBuilderFn =
+        std::function<RouteHandlerPtr(const std::string&)>;
+    using DynamicRoutePatternFn = std::function<bool(const UriSegments&)>;
+    using DynamicRouteHandlerBuilderFn =
+        std::function<RouteHandlerPtr(const RequestPtr&)>;
     using RouteMap = std::map<RoutePattern, RouteHandlerBuilderFn>;
+    using DynamicRouteMap = std::vector<
+        std::pair<DynamicRoutePatternFn, DynamicRouteHandlerBuilderFn>>;
 
   public:
     explicit Router(const RequestPtr& request);
@@ -47,12 +77,7 @@ class Router
 
     virtual ~Router() = default;
 
-    const ResponseUni& getResponse() const
-    {
-        return responseObject;
-    }
-
-    const ResponseUni& process();
+    const ResponsePtr process();
     bool preHandler();
 
     template <class THandler, typename... TArg>
@@ -60,10 +85,35 @@ class Router
     {
         static_assert(std::is_base_of_v<IRouteHandler, THandler>,
                       "Unexpected URI handler");
-        Router::routerHandlers.emplace(
-            pattern, [&](const std::string pattern) -> RouteHandlerPtr {
-                return std::make_shared<THandler>(pattern, args...);
-            });
+        const std::string uri = helpers::utils::toLower(pattern);
+        auto hashPattern = std::hash<std::string>{}(uri);
+        auto routerBuilder = [&](const std::string pattern) -> RouteHandlerPtr {
+            return std::make_shared<THandler>(pattern, args...);
+        };
+        auto isRegistered =
+            Router::routerHandlers.emplace(hashPattern, routerBuilder).second;
+        if (!isRegistered)
+        {
+            throw std::runtime_error("The route URI '" + pattern +
+                                     "' already registered");
+        }
+    }
+
+    template <class THandler>
+    static void registerDynamicUri(DynamicRoutePatternFn rule)
+    {
+        static_assert(std::is_base_of_v<IDynamicRouteHandler, THandler>,
+                      "Unexpected dynamic URI handler");
+        auto routerBuilder = [&](const RequestPtr& request) -> RouteHandlerPtr {
+            return std::make_shared<THandler>(request);
+        };
+        auto isRegistered =
+            Router::dynamicRouterHandlers.emplace_back(rule, routerBuilder).second;
+        if (!isRegistered)
+        {
+            throw std::runtime_error(
+                "The dynamic route URI already registered");
+        }
     }
 
   protected:
@@ -77,18 +127,12 @@ class Router
         return requestObject;
     }
 
-    ResponseUni& getResponse()
-    {
-        return responseObject;
-    }
-
-    void setGeneralHeaders();
-
+    void setGeneralHeaders(const ResponsePtr);
   private:
     RequestPtr requestObject;
-    ResponseUni responseObject;
 
     static RouteMap routerHandlers;
+    static DynamicRouteMap dynamicRouterHandlers;
 
     RouteHandlerPtr handler;
 };
