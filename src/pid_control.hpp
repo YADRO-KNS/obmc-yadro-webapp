cc1d5349ce2b32a5f6f817e0ae908f982f5be8cb
Igor Kononenko <i.kononenko@yadro.com>
2022-10-07 20:39:45 +0300

    entity: Add PIDProfile entity
    End-user-impact: Now GraphQL API provides PIDProfile configuration
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

--- src/pid_control.hpp
+++ src/pid_control.hpp
@@ -22,30 +22,130 @@ using namespace phosphor::logging;
 
 class PIDZone;
 
+class PIDProfile final :
+    public entity::Entity,
+    public CachedSource,
+    public NamedEntity<PIDProfile>
+{
+  public:
+    enum class Profile
+    {
+        acoustic,
+        performance
+    };
+    ENTITY_DECL_FIELD_ENUM(Profile, Profile, acoustic)
+    ENTITY_DECL_FIELD_DEF(std::vector<int>, SupportedProfiles, {})
+
+  private:
+    class Query : public dbus::FindObjectDBusQuery
+    {
+        static constexpr const char* thermalModeIface =
+            "xyz.openbmc_project.Control.ThermalMode";
+
+        class FormatProfile : public query::dbus::IFormatter
+        {
+          public:
+            ~FormatProfile() override = default;
+
+            const DbusVariantType format(const PropertyName& property,
+                                         const DbusVariantType& value) override
+            {
+                // clang-format: off
+                static const std::map<std::string, Profile> profiles{
+                    {"Acoustic", Profile::acoustic},
+                    {"Performance", Profile::performance},
+                };
+                // clang-format: on
+                return formatValueFromDict(profiles, property, value,
+                                           Profile::acoustic);
+            }
+        };
+
+        class FormatSupportedProfiles : public query::dbus::IFormatter
+        {
+          public:
+            ~FormatSupportedProfiles() override = default;
+
+            const DbusVariantType format(const PropertyName& property,
+                                         const DbusVariantType& value) override
+            {
+                std::vector<int> supportedProfiles;
+                FormatProfile formatter;
+                if (std::holds_alternative<std::vector<std::string>>(value))
+                {
+                    const auto& valueDict =
+                        std::get<std::vector<std::string>>(value);
+                    for (const auto& profile : valueDict)
+                    {
+                        auto intEnumVal =
+                            std::get<int>(formatter.format(property, profile));
+                        supportedProfiles.emplace_back(intEnumVal);
+                    }
+                }
+                return supportedProfiles;
+            }
+        };
+
+      public:
+        Query() : FindObjectDBusQuery()
+        {}
+        ~Query() override = default;
+
+        // clang-format: off
+        DBUS_QUERY_DECL_EP(DBUS_QUERY_EP_IFACES(
+            thermalModeIface,
+            DBUS_QUERY_EP_SET_FORMATTERS("Current", fieldProfile,
+                                         DBUS_QUERY_EP_CSTR(FormatProfile)),
+            DBUS_QUERY_EP_SET_FORMATTERS(
+                "Supported", fieldSupportedProfiles,
+                DBUS_QUERY_EP_CSTR(FormatSupportedProfiles))))
+
+      protected:
+        DBUS_QUERY_DECLARE_CRITERIA("/xyz/openbmc_project/control/",
+                                    DBUS_QUERY_CRIT_IFACES(thermalModeIface),
+                                    noDepth, std::nullopt)
+        // clang-format: on
+    };
+
+  public:
+    PIDProfile() : Entity(), query(std::make_shared<Query>())
+    {}
+    ~PIDProfile() override = default;
+
+  protected:
+    ENTITY_DECL_QUERY(query)
+  private:
+    DBusQueryPtr query;
+};
+
 class PID final :
     public entity::Collection,
     public CachedSource,
     public NamedEntity<PID>
 {
-    static constexpr const char* fieldName = "Name";
-    static constexpr const char* fieldClass = "Class";
-    static constexpr const char* fieldType = "Type";
-    static constexpr const char* fieldInputs = "Inputs";
-    static constexpr const char* fieldFFGainCoefficient = "FeedForwardGainCoef";
-    static constexpr const char* fieldFFOffCoefficient =
-        "FeedForwardOffsetCoef";
-    static constexpr const char* fieldICoefficient = "IntegralCoef";
-    static constexpr const char* fieldILimitMax = "IntegralLimitMax";
-    static constexpr const char* fieldILimitMin = "IntegralLimitMin";
-    static constexpr const char* fieldNegativeHysteresis = "NegativeHyst";
-    static constexpr const char* fieldPositiveHysteresis = "PositiveHyst";
-    static constexpr const char* fieldOutLimitMax = "OutputMax";
-    static constexpr const char* fieldOutLimitMin = "OutputMin";
-    static constexpr const char* fieldOutputs = "Outputs";
-    static constexpr const char* fieldPCoefficient = "ProportionalCoef";
-    static constexpr const char* fieldSlewNeg = "SlewNegative";
-    static constexpr const char* fieldSlewPos = "SlewPositive";
+  public:
+    ENTITY_DECL_FIELD(std::string, Name)
+    ENTITY_DECL_FIELD(std::string, Type)
+    ENTITY_DECL_FIELD(std::string, Class)
 
+    ENTITY_DECL_FIELD_DEF(std::vector<std::string>, Inputs, {})
+    ENTITY_DECL_FIELD_DEF(std::vector<std::string>, Outputs, {})
+
+    ENTITY_DECL_FIELD_DEF(double, FeedForwardGainCoef, 0);
+    ENTITY_DECL_FIELD_DEF(double, FeedForwardOffsetCoef, 0)
+
+    ENTITY_DECL_FIELD_DEF(double, IntegralCoef, 0)
+    ENTITY_DECL_FIELD_DEF(double, IntegralLimitMax, 0)
+    ENTITY_DECL_FIELD_DEF(double, IntegralLimitMin, 0)
+    ENTITY_DECL_FIELD_DEF(double, NegativeHyst, 0)
+    ENTITY_DECL_FIELD_DEF(double, PositiveHyst, 0)
+    ENTITY_DECL_FIELD_DEF(double, OutputMax, 0)
+    ENTITY_DECL_FIELD_DEF(double, OutputMin, 0)
+    ENTITY_DECL_FIELD_DEF(double, ProportionalCoef, 0)
+    ENTITY_DECL_FIELD_DEF(double, SlewNegative, 0)
+    ENTITY_DECL_FIELD_DEF(double, SlewPositive, 0)
+
+  private:
     class Query final : public dbus::FindObjectDBusQuery
     {
         static constexpr const char* emServiceName =
@@ -87,18 +187,18 @@ class PID final :
                 DBUS_QUERY_EP_FIELDS_ONLY2(fieldZones),
                 DBUS_QUERY_EP_FIELDS_ONLY2(fieldInputs),
                 DBUS_QUERY_EP_FIELDS_ONLY2(fieldOutputs),
-                DBUS_QUERY_EP_FIELDS_ONLY(propFFGainCoefficient, fieldFFGainCoefficient),
-                DBUS_QUERY_EP_FIELDS_ONLY(propFFOffCoefficient, fieldFFOffCoefficient),
-                DBUS_QUERY_EP_FIELDS_ONLY(propICoefficient, fieldICoefficient),
-                DBUS_QUERY_EP_FIELDS_ONLY(propILimitMax, fieldILimitMax),
-                DBUS_QUERY_EP_FIELDS_ONLY(propILimitMin, fieldILimitMin),
-                DBUS_QUERY_EP_FIELDS_ONLY(propNegativeHysteresis, fieldNegativeHysteresis),
-                DBUS_QUERY_EP_FIELDS_ONLY(propOutLimitMax, fieldOutLimitMax),
-                DBUS_QUERY_EP_FIELDS_ONLY(propOutLimitMin, fieldOutLimitMin),
-                DBUS_QUERY_EP_FIELDS_ONLY(propPCoefficient, fieldPCoefficient),
-                DBUS_QUERY_EP_FIELDS_ONLY(propPositiveHysteresis, fieldPositiveHysteresis),
-                DBUS_QUERY_EP_FIELDS_ONLY(propSlewNeg, fieldSlewNeg),
-                DBUS_QUERY_EP_FIELDS_ONLY(propSlewPos, fieldSlewPos),
+                DBUS_QUERY_EP_FIELDS_ONLY(propFFGainCoefficient, fieldFeedForwardGainCoef),
+                DBUS_QUERY_EP_FIELDS_ONLY(propFFOffCoefficient, fieldFeedForwardOffsetCoef),
+                DBUS_QUERY_EP_FIELDS_ONLY(propICoefficient, fieldIntegralCoef),
+                DBUS_QUERY_EP_FIELDS_ONLY(propILimitMax, fieldIntegralLimitMax),
+                DBUS_QUERY_EP_FIELDS_ONLY(propILimitMin, fieldIntegralLimitMin),
+                DBUS_QUERY_EP_FIELDS_ONLY(propNegativeHysteresis, fieldNegativeHyst),
+                DBUS_QUERY_EP_FIELDS_ONLY(propOutLimitMax, fieldOutputMax),
+                DBUS_QUERY_EP_FIELDS_ONLY(propOutLimitMin, fieldOutputMin),
+                DBUS_QUERY_EP_FIELDS_ONLY(propPCoefficient, fieldProportionalCoef),
+                DBUS_QUERY_EP_FIELDS_ONLY(propPositiveHysteresis, fieldPositiveHyst),
+                DBUS_QUERY_EP_FIELDS_ONLY(propSlewNeg, fieldSlewNegative),
+                DBUS_QUERY_EP_FIELDS_ONLY(propSlewPos, fieldSlewPositive),
             )
         )
 
@@ -151,9 +251,10 @@ class PID final :
         };
         return relations;
     }
+
   protected:
     ENTITY_DECL_QUERY(query)
-    ENTITY_DECL_RELATION(PIDZone, relationRules())
+    ENTITY_DECL_RELATIONS(ENTITY_DEF_RELATION(PIDZone, relationRules()))
   private:
     DBusQueryPtr query;
 };
@@ -163,10 +264,12 @@ class PIDZone final :
     public CachedSource,
     public NamedEntity<PIDZone>
 {
-    static constexpr const char* fieldName = "Name";
-    static constexpr const char* fieldFailsafePercent = "FailSafePercent";
-    static constexpr const char* fieldMinOutputPercent = "MinOutputPercent";
+  public:
+    ENTITY_DECL_FIELD(std::string, Name)
+    ENTITY_DECL_FIELD_DEF(double, MinOutputPercent, 0)
+    ENTITY_DECL_FIELD_DEF(double, FailSafePercent, 0)
 
+  private:
     class Query final : public dbus::FindObjectDBusQuery
     {
         static constexpr const char* emServiceName =
@@ -189,7 +292,7 @@ class PIDZone final :
                 pidZoneInterfaceName,
                 DBUS_QUERY_EP_FIELDS_ONLY2(fieldName),
                 DBUS_QUERY_EP_FIELDS_ONLY(
-                    propFailSafePercent, fieldFailsafePercent),
+                    propFailSafePercent, fieldFailSafePercent),
                 DBUS_QUERY_EP_FIELDS_ONLY(
                     propMinThermalOutput, fieldMinOutputPercent)
             )
@@ -212,7 +315,7 @@ class PIDZone final :
 
   protected:
     ENTITY_DECL_QUERY(query)
-    ENTITY_DECL_RELATION(PID, PID::relationRules())
+    ENTITY_DECL_RELATIONS(ENTITY_DEF_RELATION(PID, PID::relationRules()))
   private:
     DBusQueryPtr query;
 };
