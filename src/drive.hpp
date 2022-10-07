aac8f11e163383f31321a4cdddd95ad4a99723eb
Igor Kononenko <i.kononenko@yadro.com>
2022-10-07 20:38:46 +0300

    enitty: Add StorageController Entity
    End-user-impact: Now GraphQL provides StorageController inventory
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

--- src/drive.hpp
+++ src/drive.hpp
@@ -3,13 +3,12 @@
 
 #pragma once
 
-#include <phosphor-logging/log.hpp>
-
+#include <common_fields.hpp>
 #include <core/entity/dbus_query.hpp>
 #include <core/entity/entity.hpp>
-
-#include <common_fields.hpp>
 #include <formatters.hpp>
+#include <phosphor-logging/log.hpp>
+#include <status_provider.hpp>
 
 namespace app
 {
@@ -26,13 +25,45 @@ class Drive final :
     public CachedSource,
     public NamedEntity<Drive>
 {
-    static constexpr const char* fieldName = "Name";
-    static constexpr const char* fieldEnabled = "Enabled";
-    static constexpr const char* fieldCap = "Capacity";
-    static constexpr const char* fieldProto = "Protocol";
-    static constexpr const char* fieldType = "MediaType";
-    static constexpr const char* fieldUpdating = "Updating";
+  public:
+    enum class Protocol
+    {
+        sas,
+        sata,
+        nvme,
+        fc,
+        unknown
+    };
 
+    enum class MediaType
+    {
+        hdd,
+        ssd,
+        unknown
+    };
+
+    enum class State
+    {
+        enabled,
+        absent
+    };
+
+    ENTITY_DECL_FIELD(std::string, Id)
+    ENTITY_DECL_FIELD(std::string, Name)
+    ENTITY_DECL_FIELD(std::string, PrettyName)
+    ENTITY_DECL_FIELD(std::string, Manufacturer)
+    ENTITY_DECL_FIELD(std::string, Model)
+    ENTITY_DECL_FIELD(std::string, SerialNumber)
+    ENTITY_DECL_FIELD(std::string, PartNumber)
+    ENTITY_DECL_FIELD_DEF(uint64_t, Capacity, 0UL)
+    ENTITY_DECL_FIELD_ENUM(Protocol, Protocol, unknown)
+    ENTITY_DECL_FIELD_ENUM(MediaType, MediaType, unknown)
+    ENTITY_DECL_FIELD_DEF(bool, Updating, false)
+    ENTITY_DECL_FIELD_ENUM(StatusProvider::Status, Status, ok)
+    ENTITY_DECL_FIELD_ENUM(StatusProvider::Status, StatusRollup, ok)
+    ENTITY_DECL_FIELD_ENUM(State, State, absent)
+
+  private:
     class Query final : public dbus::FindObjectDBusQuery
     {
         static constexpr const char* invDriveIface =
@@ -42,10 +73,90 @@ class Drive final :
         static constexpr const char* invItemIface =
             "xyz.openbmc_project.Inventory.Item";
 
-        static constexpr const char* propName = "PrettyName";
         static constexpr const char* propPresent = "Present";
         static constexpr const char* propType = "Type";
         static constexpr const char* propRebuild = "Rebuilding";
+
+        class FormatProtocol : public query::dbus::IFormatter
+        {
+
+            static std::string dbusEnum(const std::string& val)
+            {
+                return "xyz.openbmc_project.Inventory.Item.Drive."
+                       "DriveProtocol." +
+                       val;
+            }
+
+          public:
+            ~FormatProtocol() override = default;
+
+            const DbusVariantType format(const PropertyName& property,
+                                         const DbusVariantType& value) override
+            {
+                // clang-format: off
+                static const std::map<std::string, Protocol> dict{
+                    {dbusEnum("SAS"), Protocol::sas},
+                    {dbusEnum("SATA"), Protocol::sata},
+                    {dbusEnum("NVMe"), Protocol::nvme},
+                    {dbusEnum("FC"), Protocol::fc},
+                };
+                // clang-format: on
+                return formatValueFromDict(dict, property, value,
+                                           Protocol::unknown);
+            }
+        };
+
+        class FormatMediaType : public query::dbus::IFormatter
+        {
+
+            static std::string dbusEnum(const std::string& val)
+            {
+                return "xyz.openbmc_project.Inventory.Item.Drive.DriveType." +
+                       val;
+            }
+
+          public:
+            ~FormatMediaType() override = default;
+
+            const DbusVariantType format(const PropertyName& property,
+                                         const DbusVariantType& value) override
+            {
+                // clang-format: off
+                static const std::map<std::string, MediaType> dict{
+                    {dbusEnum("HDD"), MediaType::hdd},
+                    {dbusEnum("SSD"), MediaType::ssd},
+                };
+                // clang-format: on
+                return formatValueFromDict(dict, property, value,
+                                           MediaType::unknown);
+            }
+        };
+
+        class FormatState : public query::dbus::IFormatter
+        {
+          public:
+            ~FormatState() override = default;
+
+            const DbusVariantType format(const PropertyName& property,
+                                         const DbusVariantType& value) override
+            {
+                State state = State::absent;
+                if (!std::holds_alternative<bool>(value))
+                {
+                    log<level::ERR>("Invalid type of dbus property to "
+                                    "calculate Drive::State",
+                                    entry("PROP=%s", property.c_str()));
+                }
+                else
+                {
+                    if (std::get<bool>(value))
+                    {
+                        state = State::enabled;
+                    }
+                }
+                return static_cast<int>(state);
+            }
+        };
 
       public:
         Query() : dbus::FindObjectDBusQuery()
@@ -56,37 +167,52 @@ class Drive final :
         DBUS_QUERY_DECL_EP(
             DBUS_QUERY_EP_IFACES(
                 general::assets::assetInterface,
-                DBUS_QUERY_EP_FIELDS_ONLY2(general::assets::manufacturer),
-                DBUS_QUERY_EP_FIELDS_ONLY2(general::assets::model),
-                DBUS_QUERY_EP_FIELDS_ONLY2(general::assets::serialNumber),
-                DBUS_QUERY_EP_FIELDS_ONLY2(general::assets::partNumber)),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldManufacturer),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldModel),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldSerialNumber),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldPartNumber)),
             DBUS_QUERY_EP_IFACES(invDriveIface,
-                DBUS_QUERY_EP_FIELDS_ONLY2(fieldCap),
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldCapacity),
                 DBUS_QUERY_EP_SET_FORMATTERS2(
-                    fieldProto,
-                    DBUS_QUERY_EP_CSTR(DBusEnumFormatter)),
+                    fieldProtocol,
+                    DBUS_QUERY_EP_CSTR(FormatProtocol)),
                 DBUS_QUERY_EP_SET_FORMATTERS(
-                    propType, fieldType,
-                    DBUS_QUERY_EP_CSTR(DBusEnumFormatter))),
+                    propType, fieldMediaType,
+                    DBUS_QUERY_EP_CSTR(FormatMediaType))
+            ),
             DBUS_QUERY_EP_IFACES(driveStateIface,
-                DBUS_QUERY_EP_FIELDS_ONLY(propRebuild,fieldUpdating)),
+                DBUS_QUERY_EP_FIELDS_ONLY(propRebuild, fieldUpdating)
+            ),
             DBUS_QUERY_EP_IFACES(invItemIface,
-                DBUS_QUERY_EP_FIELDS_ONLY2(fieldName),
-                DBUS_QUERY_EP_FIELDS_ONLY(propPresent, fieldEnabled)))
+                DBUS_QUERY_EP_FIELDS_ONLY2(fieldPrettyName),
+                DBUS_QUERY_EP_SET_FORMATTERS(
+                    propPresent, fieldState,
+                    DBUS_QUERY_EP_CSTR(FormatState))
+            )
+        )
 
       protected:
         DBUS_QUERY_DECLARE_CRITERIA(
             "/xyz/openbmc_project/inventory",
             DBUS_QUERY_CRIT_IFACES(invDriveIface),
-            noDepth, 
+            noDepth,
             std::nullopt
         )
         /* clang-format on */
-
+        void supplementByStaticFields(
+            const DBusInstancePtr& instance) const override
+        {
+            const auto& objectPath = instance->getObjectPath();
+            setFieldId(instance,
+                       getNameFromLastSegmentObjectPath(objectPath, false));
+            setFieldName(instance,
+                         getNameFromLastSegmentObjectPath(objectPath));
+        }
         const DefaultFieldsValueDict& getDefaultFieldsValue() const
         {
             static const DefaultFieldsValueDict defaultFields{
-                {fieldUpdating, []() { return false; }},
+                StatusRollup::defaultGetter<Drive>(),
+                StatusFromRollup::defaultGetter<Drive>(),
             };
             return defaultFields;
         }
@@ -94,13 +220,86 @@ class Drive final :
 
   public:
     Drive() : Collection(), query(std::make_shared<Query>())
-    {}
+    {
+        this->createMember(fieldId);
+        this->createMember(fieldName);
+    }
     ~Drive() override = default;
 
   protected:
     ENTITY_DECL_QUERY(query)
   private:
     DBusQueryPtr query;
+};
+
+class StorageControllers final :
+    public entity::Collection,
+    public LazySource,
+    public NamedEntity<StorageControllers>
+{
+  public:
+    ENTITY_DECL_FIELD(std::string, Id);
+    ENTITY_DECL_FIELD(std::string, PrettyName);
+    ENTITY_DECL_FIELD_ENUM(StatusProvider::Status, Status, ok)
+    ENTITY_DECL_FIELD_ENUM(Drive::State, State, absent)
+  private:
+    class Query : public IQuery, public ISingleton<Query>
+    {
+        static constexpr const char* defaultStorageId =
+            "PCH_Storage_Controller";
+        static constexpr const char* defaultStoragePrettyName =
+            "PCH Storage Controller";
+
+      public:
+        Query() = default;
+        ~Query() override = default;
+        /**
+         * @brief Run the configured quiry.
+         *
+         * @return std::vector<TInstance> The list of retrieved data which
+         *                                incapsulated at TInstance objects
+         */
+        const entity::IEntity::InstanceCollection process() override
+        {
+            const InstancePtr instance =
+                std::make_shared<StaticInstance>(defaultStorageId);
+
+            StorageControllers::setFieldId(instance, defaultStorageId);
+            StorageControllers::setFieldPrettyName(instance,
+                                                   defaultStoragePrettyName);
+            // PCH Storage Controller enabled all through a time.
+            Drive::setFieldStatus(instance, StatusProvider::Status::ok);
+            Drive::setFieldState(instance, Drive::State::enabled);
+            return {instance};
+        }
+
+        const QueryFields getFields() const override
+        {
+            QueryFields fields{
+                fieldId,
+                fieldPrettyName,
+                fieldState,
+                fieldStatus,
+            };
+
+            return std::forward<QueryFields>(fields);
+        }
+    };
+
+  public:
+    StorageControllers() : Collection(), query(std::make_shared<Query>())
+    {
+        this->initialize();
+    }
+    ~StorageControllers() override = default;
+
+  protected:
+    // Current implementation based on PCH-only storage controller.
+    // Thun, link each Drive instance to the PchControllerInstance
+    ENTITY_DECL_RELATIONS(ENTITY_DEF_RELATION_DIRECT(Drive))
+    ENTITY_DECL_QUERY(query)
+  private:
+    std::shared_ptr<Query> query;
 };
 
 } // namespace entity
