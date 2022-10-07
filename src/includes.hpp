5972541b5b60394e1c115817f2ecc69130e2fe00
Igor Kononenko <i.kononenko@yadro.com>
2022-10-07 20:30:56 +0300

    application: Changes a way to include entity hpp
    End-user-impact: None
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

---
+++ src/includes.hpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022, KNS Group LLC (YADRO)
+
+#pragma once
+
+#include <account_settings.hpp>
+#include <accounts.hpp>
+#include <baseboard.hpp>
+#include <certificates.hpp>
+#include <chassis.hpp>
+#include <cpu.hpp>
+#include <drive.hpp>
+#include <fan.hpp>
+#include <firmware.hpp>
+#include <manager.hpp>
+#include <memory.hpp>
+#include <network_adapter.hpp>
+#include <network_interface.hpp>
+#include <network_protocol.hpp>
+#include <pcie.hpp>
+#include <pid_control.hpp>
+#include <power.hpp>
+#include <psu.hpp>
+#include <roles.hpp>
+#include <sensors.hpp>
+#include <server.hpp>
+#include <session_manager.hpp>
+#include <settings.hpp>
+#include <virtual_media.hpp>
